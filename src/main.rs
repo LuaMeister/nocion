033a67e380f37ad23abc3a8c70c6e7efe7cb926f
Ryan Hollink <ryan.hollink@hotmail.com>
2023-04-06 00:40:09 +0200

    quick setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+
+struct Action {
+    name: &'static str,
+    run: fn(),
+}
+
+const ACTIONS: [Action; 1] = [
+    Action {
+        name: "generate",
+        run: || println!("Hello, world!")
+    }
+];
+
 fn main() {
-    println!("Hello, world!");
+    let action_name = std::env::args().nth(1);
+    
+    if let Some(action_name) = action_name {
+        for action in ACTIONS.iter() {
+            if action.name == action_name {
+                (action.run)();
+                return;
+            }
+        }
+    }
 }
